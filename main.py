131df1884bfe541d6fa0beee29ae2e762f9bed8b
Kirbysabamf <rluckay@gmail.com>
2021-02-18 09:38:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "Vader")
+    name = os.environ.get("NAME", "Mando")
     return "Hello Bob {}!".format(name)
 
 
